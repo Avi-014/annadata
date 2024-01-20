cb11efdb919023eac9205c1178ece5ba66e70311
Neeraj Kumar <nk4976744@gmail.com>
2024-01-20 10:37:43 +0530

    added login and register component

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -49,7 +49,6 @@ const Header = () => {
         <Link to="/login" class="text-sm font-semibold leading-6 text-white">
           Login to Contribute <span aria-hidden="true">&rarr;</span>
         </Link>
-        
       </div>
     </nav>
   );
